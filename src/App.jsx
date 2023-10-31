2380c0bb26765f37e6c0bbe954f99dc6534c3deb
grochowp <patgro3@wp.pl>
2023-10-31 23:23:54 +0100

    added first components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
-import "./App.css";
+import { Chat } from "@/components/chat";
 
 function App() {
   return (
