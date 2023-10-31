cef23b9693da6343cf1e14fd8a358ee4bbff524d
grochowp <patgro3@wp.pl>
2023-10-31 23:34:39 +0100

    fixed bug with npm run dev

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ import path, { dirname } from "path";
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [{ find: "@", replacement: path.resolve(dirname, "src") }],
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
 });
